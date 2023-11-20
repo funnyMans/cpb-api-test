c104ea08d0d0f78721ecc2f16d095babcf59c74c
Vladimir Petrosyan <vl.petrosyan94@gmail.com>
2023-11-21 02:59:13 +0400

    CPB_API/fix/init.shell touch .env file before copy

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -3,6 +3,7 @@ DOCKER_CONTEXT_DIR="./"
 
 # Copy files to the Docker context directory
 cp -r "$SOURCE_DIR"/* "$DOCKER_CONTEXT_DIR"/
+touch .env
 cp .env.example .env
 
 # Change to the Docker context directory
